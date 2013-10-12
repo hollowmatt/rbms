c82e837d55e51adde23eb360dd215341bd7da249
Matt Holloway <mholloway@book.com>
2013-10-12 16:21:19 -0700

    First commit of R2R4 framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rbms::Application.config.secret_key_base = 'fe1098f3f9ef5c0c9bdf16b5e96d795a0d5f1ded8e6955f780e40742342681f4de3abaca6b830ffdba47e41071b630cfd76267e5264bfd36af5ad41da770582e'
