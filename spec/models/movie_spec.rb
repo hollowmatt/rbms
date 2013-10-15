784adaa0d930e89d4bbffcd437f740d340bfddb1
Matt Holloway <mholloway@book.com>
2013-10-14 22:21:37 -0700

    Test for movie model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,4 +3,25 @@ require 'spec_helper'
 
 describe Movie do
 	
+	context "Sunny Day" do
+		before(:each) do
+			@movie = Movie.new
+			@movie.title = "Snatch"
+			@movie.id = 107
+			@movie.adult = false
+			@movie.status = "Released"
+		end
+
+		describe "get availablity" do
+			it "Should be available" do
+				expect(@movie.available?).to be true
+			end
+		end
+
+		describe "check age appropriateness" do
+			it "should not be adult" do
+				expect(@movie.is_adult?).to be false
+			end
+		end
+	end
 end
