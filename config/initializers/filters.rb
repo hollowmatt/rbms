6fa6b7d36625e6f2843127370de476849f9cf9f8
Matt Holloway <mholloway@book.com>
2014-02-26 16:28:16 -0800

    updated a little

---
+++ config/initializers/filters.rb
@@ -0,0 +1,10 @@
+class ActionController::Base
+
+  before_action :fubar
+
+  def fubar
+
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rbms::Application.routes.draw do
-  root :to => 'home#index'
+  root :to => 'home#index', as: :homepage
 
   get '/movies/find' => 'movies#find'
   get '/movies/:id' => 'movies#show', as: :movies_show
-  mount Wungine::Engine => "/weather"
+
+  mount Wungine::Engine, at: "weather", as: :wungine
+
 end
